f63a34982bb267302943e30d78937a7e82c7aea3
pariya-ence <pariyakorn.bua@pcru.ac.th>
2026-08-31 23:38:48 +0700

    Refactor PIR_PIN definition and clean comments
    Updated comments and defined PIR_PIN as a constant.

--- LAB_GE/LAB3.ino
+++ LAB_GE/LAB3.ino
@@ -1,32 +1,23 @@
-/*การเชื่อมต่ออุปกรณ์
-  PIR Motion Sensor
-    VCC  -> 3.3V
-    GND  -> GND
-    OUT  -> GPIO4
-  LED
-    Anode (+)   -> GPIO2
-    Cathode (-) -> ตัวต้านทาน 220Ω -> GND*/
-int PIR_PIN = 4;     // ขาสัญญาณจาก PIR Motion Sensor
-int LED_PIN = 2;     // ขาควบคุม LED
-
+/*  การเชื่อมต่อ 
+  PIR VCC  -> 3.3V
+  PIR GND  -> GND
+  PIR OUT  -> GPIO4*/
+#define PIR_PIN 4
 void setup() {
+  // เริ่มต้นการสื่อสารผ่าน Serial Monitor
   Serial.begin(115200);
-  // กำหนดโหมดการทำงานของขา GPIO
-  pinMode(PIR_PIN, INPUT);     // PIR เป็นอินพุต
-  pinMode(LED_PIN, OUTPUT);    // LED เป็นเอาต์พุต
-  // ปิด LED เมื่อเริ่มต้นโปรแกรม
-  digitalWrite(LED_PIN, LOW);
+  // กำหนดขา PIR เป็นอินพุต
+  pinMode(PIR_PIN, INPUT);
 }
 void loop() {
-  // HIGH = พบการเคลื่อนไหว
-  // LOW  = ไม่พบการเคลื่อนไหว
+  // อ่านค่าจากเซ็นเซอร์
   int motion = digitalRead(PIR_PIN);
+  // ตรวจสอบสถานะการเคลื่อนไหว
   if (motion == HIGH) {
-    digitalWrite(LED_PIN, HIGH);
     Serial.println("Motion Detected");
   } else {
-    digitalWrite(LED_PIN, LOW);
     Serial.println("No Motion");
   }
   delay(500);
 }
+
